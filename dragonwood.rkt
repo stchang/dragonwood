78d164f9f358c82a0382d8f920e99beb45560137
unknown <stchang216@gmail.com>
2020-02-15 18:44:43 -0500

    define some print fns

--- dragonwood.rkt
+++ dragonwood.rkt
@@ -6,6 +6,7 @@
 (define POINTS '(1 2 3 4 5 6 7)) ; possible point values of cards
 (define NUMSIDES (length DIE))
 
+;; prints header of each table
 (define (print-header numdice numpossible)
   (printf "\n**number of dice = ~a**, " numdice)
   (printf "total possibilities = ~a\n" numpossible)
@@ -15,7 +16,21 @@
   (display "| :---: | ---: | ---: | --- |")
   (for ([p POINTS]) (display " --- |"))
   (newline))
-  
+
+;; converts exact pts value to 2 decimal str
+(define (pts->str pts)
+  (~r (exact->inexact pts) #:precision '(= 2)))
+
+;; converts prob in the form of exact fraction to [0,100]%
+;; with precision 2
+(define (prob->str prob [width 1])
+  (~r (exact->inexact (* 100 prob)) #:precision '(= 2) #:min-width width))
+
+;; calculate expected pts per card when miss penalty is `cost`
+;; expected pts per card = expected pts / expected cards
+(define (pts/card prob pts cards [cost 1])
+  (/ (* prob pts) ; no pts for miss
+     (+ (* prob cards) (* (- 1 prob) cost))))
 
 ;; this must be macro to generate the `$n` # of for* args
 (define-syntax generate-probabilities
@@ -35,21 +50,14 @@
          (for ([i (in-range (* NUMDICE 1) (add1 (* NUMDICE 4)))])
            (define chances (count (curry <= i) rolls))
            (define prob (/ chances NUMPOSSIBLE))
-           (define prob-no (- 1 prob))
-           (printf "| >= ~a | ~a / ~a | ~a% | *pts* / *card*: |"
+           (printf "| >= ~a | ~a / ~a | ~a% | *pts / card*: |"
                    (~a i #:min-width 2)
                    (~r chances #:min-width $n)
                    NUMPOSSIBLE
-                   (~r (exact->inexact (* 100 prob))
-                       #:precision '(= 2) #:min-width 6))
+                   (prob->str prob))
            (for ([pts POINTS])
-             (printf "~a |"
-                     ; expected pts per card = expected pts / expected cards
-                     (~r (exact->inexact
-                          (/ (* prob pts)
-                             ;; num cards = NUMDICE; miss costs 1 card
-                             (+ (* prob NUMDICE) prob-no)))
-                         #:precision '(= 2) #:min-width 4)))
+             ;; num cards = NUMDICE; miss costs 1 card
+             (printf " ~a |" (pts->str (pts/card prob pts NUMDICE))))
            (newline)))]))
                    
 
@@ -64,15 +72,10 @@
   (λ ()
     (printf "# Dice probabilities for the game Dragonwood\n\n")
     (printf "Each die has sides: 1, 2, 2, 3, 3, 4\n\n")
-    (let* ([dragon-prob (/ 13 36)] [dragon-prob-no (- 1 dragon-prob)])
+    (let ([dragon-prob (/ 13 36)])
       (printf "Dragon Spell = ~a% to get 6 or 7 pts = ~a or ~a pts/card\n"
-              (~r (exact->inexact (* 100 dragon-prob)) #:precision '(= 2))
-              (~r (exact->inexact
-                   (/ (* dragon-prob 6)
-                      (+ (* dragon-prob 3) (* dragon-prob-no 2))))
-                  #:precision '(= 2))
-              (~r (exact->inexact
-                   (/ (* dragon-prob 7)
-                      (+ (* dragon-prob 3) (* dragon-prob-no 2))))
-                  #:precision '(= 2))))
+              (prob->str dragon-prob)
+              (pts->str (pts/card dragon-prob 6 3 2))
+              (pts->str (pts/card dragon-prob 7 3 2))))
     (gen-all #:max-dice 6)))
+)
